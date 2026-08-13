6900215bb63f68d655241f9def62129f9d9df40c
Samuel <168583510+Slxz00@users.noreply.github.com>
2026-08-12 22:03:58 -0300

    Add SQL file with DB Fiddle links

---
+++ joins-assinantes.sql
@@ -0,0 +1,5 @@
+https://www.db-fiddle.com/f/fygDdhMQuuDf2pVbg1j9JG/0
+https://www.db-fiddle.com/f/dbeDtzZUfxNrGsoUfNZadr/0
+https://www.db-fiddle.com/f/dbeDtzZUfxNrGsoUfNZadr/1
+https://www.db-fiddle.com/f/qdPoboVmdT56yZaaBAt9VP/0
+https://www.db-fiddle.com/f/qdPoboVmdT56yZaaBAt9VP/1
